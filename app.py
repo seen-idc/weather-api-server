376349ad58a0943a164572249471e49fe401dde6
seen-idc <seen.idc@gmail.com>
2021-12-23 17:20:47 +1100

    ok

--- app.py
+++ app.py
@@ -27,14 +27,14 @@ def ruse():
             req = requests.get(query)
             last_api_call_data['carlingford_nsw_au'] = req.text
 
-            return jsonify(req.text)
+            return req.text
         else:
-            return jsonify(last_api_call_data['carlingford_nsw_au'])
+            return last_api_call_data['carlingford_nsw_au']
     else:
         last_api_call['carlingford_nsw_au'] = time.time()
         req = requests.get(query)
         last_api_call_data['carlingford_nsw_au'] = req.text
-        return jsonify(req.text)
+        return req.text
 
 if __name__ == "__main__":
     print('Starting server')
